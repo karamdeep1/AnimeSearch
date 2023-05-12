a244726dfb2e23e2b38ca85c0b7224723e9cb26c
karamdeepbassi <ricardo036942@gmail.com>
2023-05-12 19:46:38 -0400

    unversioned files

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/main.cpp.obj"
+  "CMakeFiles/untitled.dir/main.cpp.obj.d"
+  "CMakeFiles/untitled.dir/sqlite/sqlite3.c.obj"
+  "CMakeFiles/untitled.dir/sqlite/sqlite3.c.obj.d"
+  "libuntitled.dll.a"
+  "untitled.exe"
+  "untitled.exe.manifest"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
